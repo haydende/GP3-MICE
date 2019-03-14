1773eec784581ee29bbcd1e1024a42cd368a8dbc
Esteban Giacobbe <estegiacobbe95@libero.it>
2019-03-14 13:06:53 +0000

    Home page edit
    Navigation bar customized for all the pages, added some divs and images. CSS have been implemented for the layout of the webpage.

--- EasyPHP_v2/data/localweb/MICE/application/views/header.php
+++ EasyPHP_v2/data/localweb/MICE/application/views/header.php
@@ -3,19 +3,35 @@
 <head>
 	<meta charset="utf-8" />
 	<style>
-	#nav { font-family: Arial; font-size: 14px; width: 100%; float: left; margin: 0 0 1em 0; padding: 0; list-style: none;}
+	#nav { font-family: Arial; font-size: 14px; width: 100%; float: left; margin: 0 0 1em 0; padding: 0; list-style: none;
+		background-color: #5F6980}
+		
 	#nav {list-style: none; border:0;}
 	#rightnav { list-style: none; }
 	#nav li { float: left; }
 	#rightnav li { float: right; }
-	#nav li a { margin: 0 3px 0 0; font-size: 15px; display: block; padding: 8px 15px; text-decoration: none; color: #000; background-color: grey; border: 1px solid #c1c1c1;}
-	#nav li a:hover {background-color: #f2e4d5;}
-	#nav a:link, a:visited {border-radius: 12px 12px 12px 12px; }		
+	#nav li a { margin: 0 0 0 0; font-size: 15px; display: block; padding:14px 16px; text-decoration: none; color: #FFFFFF; background-color: #5F6980; border: 1px solid #5F6980;}
+	#nav li a:hover {background-color: #354263;}
+	#nav a:link, a:visited {border-radius: 0px 0px 0px 0px; }	
+		
+#container {
+	width: 90%;
+	margin-left: auto;
+	margin-right: auto;
+	background-color: #D5D5D5;
+	
+}	
+		
+		
+		
 	</style>
 </head>
 
 <body>
-	<div>
+	
+	
+	
+	<div id="container">
 		<ul id="nav">
 		<li><a href='<?php echo site_url('')?>'>Home</a></li>
 		<li><a href='<?php echo site_url('main/cinema')?>'>MICE-OFF Cinemas</a></li>
--- EasyPHP_v2/data/localweb/MICE/application/views/home.php
+++ EasyPHP_v2/data/localweb/MICE/application/views/home.php
@@ -3,23 +3,116 @@
 <head>
 	<meta charset="utf-8" />
 	<style>
+		
+body {
+	font-family: source-sans-pro;
+	background-color: #f2f2f2;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	font-style: normal;
+	font-weight: 200;
+}
+		
+		
 		h1 {text-align: center; font-family: Calibri; color: black; font-size: 45pt; padding-bottom: 0.5px}
 		h2 {text-align: center; font-family: Calibri; color: black; font-size: 45pt; padding-top: 0.5px}
 		p.p-centre { text-align: center; font-family: Arial; }
-		#reel { display: block; padding-top: 40px; margin-left: auto; margin-right: auto; border: bold }		
+		#reel { display: block; padding-top: 40px; margin-left: auto; margin-right: auto; border: bold }	
+		
+#container {
+	width: 90%;
+	margin-left: auto;
+	margin-right: auto;
+	background-color: #FFFFFF;
+}
+		
+.stats {
+	color: #717070;
+	margin-bottom: 5px;
+}
+.gallery {
+	clear: both;
+	display: inline-block;
+	width: 100%;
+	background-color: #FFFFFF;
+	/* [disabled]min-width: 400px;
+*/
+	padding-bottom: 35px;
+	padding-top: 0px;
+	margin-top: -5px;
+	margin-bottom: 0px;
+}
+.thumbnail {
+	width: 25%;
+	text-align: center;
+	float: left;
+	margin-top: 35px;
+}
+.gallery .thumbnail h4 {
+	margin-top: 5px;
+	margin-right: 5px;
+	margin-bottom: 5px;
+	margin-left: 5px;
+	color: #52BAD5;
+}
+.gallery .thumbnail p {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	color: #A3A3A3;
+}
+		
+		
+	
 	</style>
 </head>
 <body>
 
-<div align="center">
+
+	<div id="container">
+	<div align="center">
 	<img id="MICE_LOGO" src="assets/images/MICE_LOGO.jpg" alt="MICE_LOGO" height="260" width="380">
+	</div>
+	
+
+	<h1>Official MICE old film festival website</h1>
+
+	<p class="p-centre">This site offers a range of functionality</p>
+	<p class="p-centre">Click one of the navigation links to begin.</p>
+	
+	
+	
+ <!-- Stats Gallery Section -->
+  <div class="gallery">
+    <div class="thumbnail">
+      <img class="poster" src="assets/images/salt-poster.jpg" style="width:50%">
+      <h4>TITLE</h4>
+      <p>One line description</p>
+    </div>
+    <div class="thumbnail">
+      <img class="poster" src="assets/images/Captain-Marvel-poster.jpg" style="width:50%">
+      <h4>TITLE</h4>
+      <p>One line description</p>
+    </div>
+    <div class="thumbnail">
+      <img class="poster" src="assets/images/avengers-poster.jpg" style="width:50%">
+      <h4>TITLE</h4>
+      <p>One line description</p>
+    </div>
+    <div class="thumbnail">
+      <img class="poster" src="assets/images/kristy-poster.jpg" style="width:50%">
+      <h4>TITLE</h4>
+      <p>One line description</p>
+    </div>
+  </div>
+		
 </div>
+	
+	
 
-<h1>Official MICE old film festival website</h1>
-
-<p class="p-centre">This site offers a range of functionality</p>
-<p class="p-centre">Click one of the navigation links to begin.</p>
-
-
+	
 </body>
 </html>
