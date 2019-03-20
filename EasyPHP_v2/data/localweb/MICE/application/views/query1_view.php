f597fd050db78985ad74307b927d3b3f41943cb3
Hayden Eastwell <haydeneastwell@outlook.com>
2019-03-20 16:07:23 +0000

    [Jason] Added search feature to Query 1
    Added a new search feature for Query View 1 (All cinemas) that allows you to search by Cinema name and location (yet to add for Address and Manager)

--- EasyPHP_v2/data/localweb/MICE/application/views/query1_view.php
+++ EasyPHP_v2/data/localweb/MICE/application/views/query1_view.php
@@ -11,15 +11,51 @@
 </head>
 <body>
 
+
+
 <h1>Queries</h1>
 <div align='center'>
 	<button type="submit" onclick="location.href='<?php echo site_url('main/querynav')?>'">Back to queries</button>
 </div>
 
 
+
 <h2>List of current MICE cinemas</h2>
-<div align='center'>
+
+
+<div align='center'
+<body>
+      <form method="POST" action = "<?php $_PHP_SELF ?>">
+	  <br>
+         <input type = "text" name = "cinemaname" placeholder = "Cinema Name" />
+         <input type = "text" name = "cinema_location" placeholder = "Cinema Location" />
+		 <input type = "text" name = "cinema_address" placeholder = "Cinema Address" />
+		 <input type = "text" name = "cinema_manager" placeholder = "Cinema Manager" />
+		 <input style="height:24px" type="submit" id = "submitbutton" name="submitbutton">
+      </form>
+	  <br>
+   </body> 
+
 <?php
+	if(isset($_POST['cinemaname']) || isset($_POST['cinema_location'])){
+	$varname = $_POST["cinemaname"];
+	$varname2 = $_POST["cinema_location"];
+	$tmpl = array ('table_open' => '<table class="mytable">');
+	$this->table->set_template($tmpl); 
+	
+	// if a temp table currently exists, drop it so we can create a new one.
+	$this->db->query('drop table if exists temp');
+	
+	//Create a temp table and specify required columns. 
+	$this->db->query('create temporary table temp as (select Cinema_ID, Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager FROM cinema WHERE Cinema_Name = "' .$varname. '" OR Cinema_Location = "' .$varname2. '")');
+	
+	//Present all in the temp table with a SQL select all command. 
+	$query = $this->db->query('select Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager from temp');
+	
+	echo $this->table->generate($query);
+	}   
+	
+	else{
 	$tmpl = array ('table_open' => '<table class="mytable">');
 	$this->table->set_template($tmpl); 
 	
@@ -30,10 +66,13 @@
 	$this->db->query('create temporary table temp as (select Cinema_ID, Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager FROM cinema)');
 	
 	//Present all in the temp table with a SQL select all command. 
-	$query = $this->db->query('select Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager from temp;');
+	$query = $this->db->query('select Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager from temp');
 	
 	echo $this->table->generate($query);
+	}
+	
 ?>
 </div>
+
 </body>
 </html>
