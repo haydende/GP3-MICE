444d3a4ad343fbb201d26bac17079fa945847758
Hayden Eastwell <haydeneastwell@outlook.com>
2019-03-26 18:48:44 +0000

    Begun Booking Process
    - Begun process for placing bookings 
    - Broken because of line 60, try opening the page to see details

--- EasyPHP_v2/data/localweb/MICE/application/views/make_booking_view.php
+++ EasyPHP_v2/data/localweb/MICE/application/views/make_booking_view.php
@@ -21,11 +21,9 @@
 		  <form method="POST" action = "<?php $_PHP_SELF ?>">
 		  <br><br><br><br><br><br><br><br><br><br><br><br>
 			<h4>Please input booking details</h4>
-			 <input type ="text" size="25" name="cinema_name" placeholder = "Cinema Name" style="text-align: center; font-weight: bold"" /> <br><br>
-			 <input type ="text" size="25" name="film_name" placeholder = "Film Name" style="text-align: center; font-weight: bold"" /> <br><br>
-			 <input type ="text" size="25" name="screen_number" placeholder = "Screen Number" style="text-align: center; font-weight: bold"" /> <br><br>
-			 <input type ="text" size="25" name="date" placeholder = "Performance Date" style="text-align: center; font-weight: bold" /> <br><br>
-			 <input type ="text" size="25" name="time" placeholder = "Performance Time" style="text-align: center; font-weight: bold"" /> <br><br>
+			 <input type ="text" size="25" name="Performance_ID" placeholder = "Enter the performance number..." style="text-align: center;"" /> <br><br>
+			 <input type ="text" size="25" name="Seats" placeholder = "Enter the amount of seats..." style="text-align: center;"" /> <br><br>
+			 <input type ="text" size="25" name="Member_ID" placeholder = "Enter your Member ID..." style="text-align: center;"" /> <br><br>
 			 <input style="height:24px; width:207px" type="submit" id = "submitbutton" name="submitbutton" value="Make Booking"> <br><br>
 		  </form>
 		  <br>
@@ -40,82 +38,53 @@
 
 <div style="; float:left;  margin-left:300px" >
 	<?php
-	if(isset($_POST['cinema_name']) || isset($_POST['film_name']) || isset($_POST['screen_number']) || isset($_POST['date']) || isset($_POST['time']) || isset($_POST['seats']) ){
-	
-	$cinema_name = "1";
-	$film_name = "1";
-	$screen_number = "1";
-	$date = "1";
-	$time = "1";
-	$seats = "1";
-	
-	// IF STATEMENTS TO CHECK IF FORMS HAVE BEEN FILLED BY USER AND SUBMITTED. 
-	if ($_POST['cinema_name'] != "") {
-		$cinema_name = "Cinema_Name = \"" .$_POST['cinema_name']. "\"";
-	}
-	if ($_POST['film_name'] != "") {
-		$film_name = "Title = \"" .$_POST['film_name']. "\"";
-	}
-	if ($_POST['screen_number'] != "") {
-		$screen_number = "Screen_ID = \"" .$_POST['screen_number']. "\"";
-	}
-	if ($_POST['date'] != "") {
-		$date = "Date = \"" .$_POST['date']. "\"";
-	}
-	
-	if ($_POST['time'] != "") {
-		$time = "Time = \"" .$_POST['time']. "\"";
-	}
-	
-	if ($_POST['seats'] != "") {
-		$seats = "Remaining_seats = \"" .$_POST['seats']. "\"";
-	}
-	
-	$tmpl = array ('table_open' => '<table class="mytable">');
-	$this->table->set_template($tmpl); 
-	
-	// if a temp table currently exists, drop it so we can create a new one.
-	$this->db->query('drop table if exists temp');
-	
-	//Create a temp table and specify required columns. 
-	
-	// create string containing SQL statement (used with following print for debugging)
-	//$sql_query = 'create temporary table temp as (SELECT Cinema_ID, Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager 
-													 // FROM cinema 
-													//  WHERE ' .$cinema_name. ' AND ' .$cinema_location. ' AND ' .$cinema_address. ' AND ' .$cinema_manager. ')';
-	
-	//print the SQL statement (for debugging)	  
-	// print $sql_query;
+		if (isset($_POST['Performance_ID']) && isset($_POST['Seats']) && isset($_POST['Member_ID'])) {
+		
+			$Performance_ID = $_POST['Performance_ID'];
+			$Seats = $_POST['Seats'];
+			$Member_ID = $_POST['Member_ID'];
+			
+			
+			
+			// create string containing SQL statement (used with following print for debugging)
+			//$sql_query = 'create temporary table temp as (SELECT Cinema_ID, Cinema_Name, Cinema_Location, Cinema_Address, Cinema_Manager 
+															 // FROM cinema 
+															//  WHERE ' .$cinema_name. ' AND ' .$cinema_location. ' AND ' .$cinema_address. ' AND ' .$cinema_manager. ')';
+			
+			//print the SQL statement (for debugging)	  
+			// print $sql_query;
 
-	$this->db->query('create temporary table temp as (UPDATE c.Cinema_Name, f.Title AS \'Film Title\', p.Screen_ID AS \'Screen Number\', p.Date, p.Time, p.Remaining_seats 
-													  FROM performance p, film f, cinema c 
-													  WHERE f.Film_ID = p.Film_ID AND c.Cinema_ID = p.Cinema_ID AND ' .$cinema_name. ' AND ' .$film_name. ' AND ' .$screen_number. ' AND ' .$date. 
-													  ' AND ' .$time. ' AND ' .$seats. ')');
-	
-	//Present all in the temp table with a SQL select all command. 
-	$query = $this->db->query('SELECT * FROM temp');
-	
-	echo $this->table->generate($query);
-	}   
-	
-	
-	// IF NONE OF THE FORM INPUT FIELDS HAVE BEEN SUBMITTED, DEFAULT TABLE IS DISPLAYED. 
-	else{
-	$tmpl = array ('table_open' => '<table class="mytable">');
-	$this->table->set_template($tmpl); 
-	
-	// if a temp table currently exists, drop it so we can create a new one.
-	$this->db->query('drop table if exists temp');
-	
-	//Create a temp table and specify required columns. 
-	$this->db->query('create temporary table temp as (select c.Cinema_Name, f.Title AS \'Film Title\', p.Screen_ID AS \'Screen Number\', p.Date, p.Time,  
-													  p.Remaining_seats FROM performance p, film f, cinema c WHERE f.Film_ID = p.Film_ID AND c.Cinema_ID = p.Cinema_ID)');
-	
-	//Present all in the temp table with a SQL select all command. 
-	$query = $this->db->query('select * from temp');
-	
-	echo $this->table->generate($query);
-	}
+			$this->db->query('INSERT INTO Bookings (No_seats, Member_ID, Performance_ID, Film_ID)
+							  VALUES (' .$Performance_ID. ', ' .$Seats. ', ' .$Member_ID ', (SELECT Film_ID FROM Performance WHERE Performance_ID = ' .$Performance_ID. '));');
+			print "Your booking has been placed!";
+							  
+		} else {
+			
+			$tmpl = array ('table_open' => '<table class="mytable">');
+			$this->table->set_template($tmpl); 
+			
+			// if a temp table currently exists, drop it so we can create a new one.
+			$this->db->query('DROP TABLE IF EXISTS temp');
+			
+			//Create a temp table and specify required columns. 
+			$this->db->query('CREATE TEMPORARY TABLE temp AS (SELECT p.Performance_ID AS \'#\', 
+																	 c.Cinema_Name AS \'Cinema\',
+																	 f.Title AS \'Film Title\', 
+																	 p.Screen_ID AS \'Screen\', 
+																	 p.Date AS \'Date\', 
+																	 p.Time AS \'Time\',  
+																	 p.Remaining_seats AS \'Seats Remaining\'
+															  FROM performance p, film f, cinema c 
+															  WHERE f.Film_ID = p.Film_ID AND 
+																	c.Cinema_ID = p.Cinema_ID)
+															  ORDER BY p.Date ASC, p.Time ASC');
+			
+			//Present all in the temp table with a SQL select all command. 
+			$query = $this->db->query('select * from temp');
+			
+			echo $this->table->generate($query);
+			
+		}
 	?>	
 </div>
 
